ac27eea52ed1a4b63a1b23b5cd1ce980f2f9de08
orchsik <orchsik@naver.com>
2022-02-04 22:02:02 +0900

    Add: base Sorter class

--- exam/src/Sorter.ts
+++ exam/src/Sorter.ts
@@ -1,17 +1,22 @@
 export class Sorter {
-  collection: number[];
-  constructor(collection: number[]) {
-    this.collection = collection;
+  constructor(public collection: number[]) {}
+
+  compare(leftIndex: number, rightIndex: number): boolean {
+    return this.collection[leftIndex] > this.collection[rightIndex];
+  }
+
+  swap(leftIndex: number, rightIndex: number): void {
+    const leftHand = this.collection[leftIndex];
+    this.collection[leftIndex] = this.collection[rightIndex];
+    this.collection[rightIndex] = leftHand;
   }
 
   sort(): void {
     const { length } = this.collection;
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const leftHand = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = leftHand;
+        if (this.compare(j, j + 1)) {
+          this.swap(j, j + 1);
         }
       }
     }
--- exam/src/index.ts
+++ exam/src/index.ts
@@ -1,5 +1,6 @@
 import { Sorter } from "./Sorter";
 
 const sorter = new Sorter([3, 0, 100, -2]);
+// const sorter = new Sorter("cba");
 sorter.sort();
 console.log(sorter.collection);
