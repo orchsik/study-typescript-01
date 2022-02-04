c1a352c57e5e743929e26f5a1f1e34fa53e5de60
orchsik <orchsik@naver.com>
2022-02-04 19:31:03 +0900

    Add: CharactersCollection

--- 03_sort/build/index.js
+++ 03_sort/build/index.js
@@ -1,8 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const Sorter_1 = require("./Sorter");
-const NumbersCollection_1 = require("./NumbersCollection");
-const numbersCollection = new NumbersCollection_1.NumbersCollection([100000000, 3, -5, 0]);
-const sorter = new Sorter_1.Sorter(numbersCollection);
+const CharactersCollection_1 = require("./CharactersCollection");
+// const numbersCollection = new NumbersCollection([100000000, 3, -5, 0]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+const chractersCollection = new CharactersCollection_1.CharactersCollection("Xaayb");
+const sorter = new Sorter_1.Sorter(chractersCollection);
 sorter.sort();
-console.log(numbersCollection.data);
+console.log(chractersCollection.data);
--- 03_sort/src/index.ts
+++ 03_sort/src/index.ts
@@ -1,7 +1,13 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
-const numbersCollection = new NumbersCollection([100000000, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
+// const numbersCollection = new NumbersCollection([100000000, 3, -5, 0]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+
+const chractersCollection = new CharactersCollection("Xaayb");
+const sorter = new Sorter(chractersCollection);
 sorter.sort();
-console.log(numbersCollection.data);
+console.log(chractersCollection.data);
