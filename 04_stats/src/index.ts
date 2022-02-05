51e450642c3b0bbdcff6b32d7c23383df2dcc2ad
orchsik <orchsik@naver.com>
2022-02-05 09:52:31 +0900

    Refact: magic string comparisons using enum

--- 04_stats/src/index.ts
+++ 04_stats/src/index.ts
@@ -7,25 +7,33 @@ const matches = fs
   .split("\n")
   .map((row: string): string[] => row.split(","));
 
-/**
- * [ Issues ]
- * Magic string comparisons & Source of data is hardcoded
- * Data array is all strings, even though it might have numbers in it
- * Variable named after a specific team
- * Analysis type is fixed
- * No ability to output the report in diffrent formats
+/** Issues
+ * [x] Magic string comparisons -> enum
+ * [ ] Source of data is hardcoded
+ * [ ] Data array is all strings, even though it might have numbers in it
+ * [ ] Variable named after a specific team
+ * [ ] Analysis type is fixed
+ * [ ] No ability to output the report in diffrent formats
  */
 
-const homeWin = "H";
-const awayWin = "A";
-const draw = "D";
+/** enum - enumeration
+ * Follow near-identical syntax rules as normal objects
+ * Creates an object with the same keys and values when converted from TS to JS
+ * Primary goal is to signal to other engineers that these are all closely related values
+ * Use whenever we have small fixed set of values that are all closely related and known at complie time
+ */
+enum MatchResult {
+  HomeWin = "H",
+  AwayWin = "A",
+  Draw = "D",
+}
 
 let manUnitedWins = 0;
 
 for (let match of matches) {
-  if (match[1] === "Man United" && match[5] === homeWin) {
+  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
-  } else if (match[2] === "Man United" && match[5] === awayWin) {
+  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
     manUnitedWins++;
   }
 }
