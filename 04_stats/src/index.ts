7312c26eb9278ae249ab5b251828e94c5fe828d5
orchsik <orchsik@naver.com>
2022-02-05 09:26:51 +0900

    Feat: manUnitedWins analysis (with issues)

--- 04_stats/src/index.ts
+++ 04_stats/src/index.ts
@@ -7,4 +7,24 @@ const matches = fs
   .split("\n")
   .map((row: string): string[] => row.split(","));
 
-console.log(matches);
+/**
+ * [ Issues ]
+ * Magic string comparisions
+ * Source of data is hardcoded
+ * Data array is all strings, even though it might have numbers in it
+ * Variable named after a specific team
+ * Analysis type is fixed
+ * No ability to output the report in diffrent formats
+ */
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (match[1] === "Man United" && match[5] === "H") {
+    manUnitedWins++;
+  } else if (match[2] === "Man United" && match[5] === "A") {
+    manUnitedWins++;
+  }
+}
+
+console.log(manUnitedWins);
