716e35d3c3aa31f063a298038bc5cfd8076611a1
orchsik <orchsik@naver.com>
2022-02-05 12:41:42 +0900

    Refact: non-resuable CsvFileReader by Composition

--- 04_stats/MEMO.md
+++ 04_stats/MEMO.md
@@ -12,3 +12,27 @@
 - Creates an object with the same keys and values when converted from TS to JS
 - Primary goal is to signal to other engineers that these are all closely related values
 - Use whenever we have small fixed set of values that are all closely related and known at complie time
+
+## Inheritance vs Composition
+
+창문은 너비 높이 넓이 창문열기를 갖는다.
+너비 높이 넓이는 사각형의 속성이다.
+[x] 창문은 사각형 이다. (항상 사각형 이다.)
+[o] 창문은 사각형 속성을 갖는다.(가끔 사각형 이다.)
+네모난 창문/동그란 창문을 만들 때,
+inheritance 개념으로 만들면 두개의 클래스가 만들어지고 창문열기 코드가 중복된다.
+Composition 개념으로 만들면 창문이라는 클래스를 만들 때 동그라미, 사각형 인스턴스를 전달하면 되어 하나의 클래스로 가능하며 중복이없다.
+창문과 사각형의 관계는 Compsition이 더 적절하다.
+
+MatchReader와 CsvFileReader의 관계도 마찬가지이다.
+MatchReader는 ApiReader를 가질 수도 있다.
+[x] MatchReader는 CsvFileReader 이다.
+[o] MatchReader는 CsvFileReader 속성를 갖는다.
+
+### Inheritance
+
+"is a"
+
+### Composition
+
+"has a"
--- 04_stats/src/index.ts
+++ 04_stats/src/index.ts
@@ -1,15 +1,20 @@
 import { MatchReader } from "./MatchReader";
 import { MatchResult } from "./MatchResult";
+import { CsvFileReader } from "./CsvFileReader";
 
-const reader = new MatchReader("football.csv");
-reader.read();
+// Create an object that satisfies the 'DataReader' interface
+const csvFileReader = new CsvFileReader("football.csv");
+
+// Create an instance of MatchReader and pass in something satisfying the 'DataReader' inerface
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
 /** Issues
  * [x] Magic string comparisons -> enum
  * [x] Source of data is hardcoded -> CsvFileReader
  * [x] Data array is all strings, even though it might have numbers in it -> fix CsvFileReader
-      [x] Non-reusable CsvFileReader(csv file diffrent format) -> using abstract class (abstract class CsvFileReader + class MatchResult)
-      [ ] Non-reusable CsvFileReader(csv file diffrent format) ->  
+      [x] Non-reusable CsvFileReader(csv file diffrent format) -> using inheritance (abstract class CsvFileReader + class MatchResult)
+      [x] Non-reusable CsvFileReader(csv file diffrent format) -> using composition (class CsvFileReader + class MatchReader)
  * [ ] Variable named after a specific team
  * [ ] Analysis type is fixed
  * [ ] No ability to output the report in diffrent formats
@@ -17,7 +22,7 @@ reader.read();
 
 let manUnitedWins = 0;
 
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
