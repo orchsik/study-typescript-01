07c6578b1ad1638b48deabfa5034e565ef2cc36e
orchsik <orchsik@naver.com>
2022-02-05 11:23:19 +0900

    Refact: non-reusable CsvFileReader By Abstract Class

--- 04_stats/src/index.ts
+++ 04_stats/src/index.ts
@@ -1,14 +1,15 @@
-import { CsvFileReader } from "./CsvFileReader";
+import { MatchReader } from "./MatchReader";
 import { MatchResult } from "./MatchResult";
 
-const reader = new CsvFileReader("football.csv");
+const reader = new MatchReader("football.csv");
 reader.read();
 
 /** Issues
  * [x] Magic string comparisons -> enum
  * [x] Source of data is hardcoded -> CsvFileReader
  * [x] Data array is all strings, even though it might have numbers in it -> fix CsvFileReader
-      [ ] Non-reusable CsvFileReader(csv file diffrent format)
+      [x] Non-reusable CsvFileReader(csv file diffrent format) -> using abstract class (abstract class CsvFileReader + class MatchResult)
+      [ ] Non-reusable CsvFileReader(csv file diffrent format) ->  
  * [ ] Variable named after a specific team
  * [ ] Analysis type is fixed
  * [ ] No ability to output the report in diffrent formats
