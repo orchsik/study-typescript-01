ce80b56edfc48e8566adbddd62950c77cec2aee0
orchsik <orchsik@naver.com>
2022-02-12 14:26:27 +0900

    Feat: parsing from body

--- 06_server/src/index.ts
+++ 06_server/src/index.ts
@@ -1,14 +1,11 @@
+import bodyParser from "body-parser";
 import express, { Request, Response } from "express";
+import { router } from "./routes/loginRoutes";
 
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send(`
-    <div>
-      <h1>Hi there!</h1>
-    </div>
-  `);
-});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(router);
 
 app.listen(3000, () => {
   console.log("Listening on port http://localhost:3000");
---
+++ 06_server/src/routes/loginRoutes.ts
@@ -0,0 +1,37 @@
+import { Request, Response, Router } from "express";
+
+const router = Router();
+
+router.get("/", (req: Request, res: Response) => {
+  res.send(`
+    <div>
+      <h1>Hi there!</h1>
+    </div>
+  `);
+});
+
+router.get("/login", (req: Request, res: Response) => {
+  res.send(`
+    <form method="post">
+      <div>
+        <label>Email</label>
+        <input name="email" />
+      </div>
+      <div>
+        <label>Password</label>
+        <input name="password" type="password" />
+      </div>
+      <button>Submit</button>
+    </form>
+  `);
+});
+
+router.post("/login", (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  res.send({
+    email,
+    password,
+  });
+});
+
+export { router };
