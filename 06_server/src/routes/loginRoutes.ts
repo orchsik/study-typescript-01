f3e0fceb567deff27ac5d1757938cfe3f17e12a5
orchsik <orchsik@naver.com>
2022-02-12 17:39:39 +0900

    Feat: login logout

--- 06_server/src/routes/loginRoutes.ts
+++ 06_server/src/routes/loginRoutes.ts
@@ -1,9 +1,19 @@
-import { Request, Response, Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 
 interface RequestWithBody extends Request {
   body: {
     [key: string]: string | undefined;
   };
+}
+
+function requireAuth(req: Request, res: Response, next: NextFunction): void {
+  if (req.session?.loggedIn) {
+    next();
+    return;
+  }
+
+  res.status(403);
+  res.send("Not permitted");
 }
 
 const router = Router();
@@ -51,6 +61,15 @@ router.get("/", (req: Request, res: Response) => {
     </div>
   `);
   }
+});
+
+router.get("/logout", (req: Request, res: Response) => {
+  req.session = undefined;
+  res.redirect("/");
+});
+
+router.get("/protected", requireAuth, (req: Request, res: Response) => {
+  res.send("Welcome to protected route, logged in well");
 });
 
 export { router };
