c919ef6cd30a8b31cde7f5f40f8acd0a54425dfa
orchsik <orchsik@naver.com>
2022-02-15 07:37:38 +0900

    Add: static express router

--- 06_server/src/index.ts
+++ 06_server/src/index.ts
@@ -3,15 +3,15 @@ import bodyParser from "body-parser";
 import cookieSession from "cookie-session";
 
 import { router } from "./routes/loginRoutes";
-import { router as controlerRouter } from "./controllers/decorators";
 import "./controllers/LoginController";
+import { AppRouter } from "./AppRouter";
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ["aaa"] }));
 app.use(router);
-app.use(controlerRouter);
+app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
   console.log("Listening on port http://localhost:3000");
