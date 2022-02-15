97ceb08d75dd803f67dd069a605c863a2fa4fa98
orchsik <orchsik@naver.com>
2022-02-15 20:43:36 +0900

    Feat: add factroy and enum for router method (routeBinder)

--- 06_server/src/controllers/decorators/controller.ts
+++ 06_server/src/controllers/decorators/controller.ts
@@ -1,16 +1,32 @@
 import "reflect-metadata";
 import { AppRouter } from "../../AppRouter";
+import { MetadataKeys } from "./MetadataKeys";
+import { Methods } from "./Methods";
 
 export function controller(routePrefix: string) {
   return function (target: Function) {
     const router = AppRouter.getInstance();
 
+    /**
+     * target: [Function: LoginController]
+     * target.prototype: { getLogin: [Function (anonymous)], ...}
+     * key: 'getLogin' ...
+     */
     for (let key in target.prototype) {
       const routeHandler = target.prototype[key];
-      const path = Reflect.getMetadata("path", target.prototype, key);
+      const path = Reflect.getMetadata(
+        MetadataKeys.path,
+        target.prototype,
+        key
+      );
+      const method: Methods = Reflect.getMetadata(
+        MetadataKeys.method,
+        target.prototype,
+        key
+      );
 
       if (path) {
-        router.get(`${routePrefix}${path}`, routeHandler);
+        router[method](`${routePrefix}${path}`, routeHandler);
       }
     }
   };
--- 06_server/src/controllers/decorators/routes.ts
+++ 06_server/src/controllers/decorators/routes.ts
@@ -1,7 +1,22 @@
 import "reflect-metadata";
+import { MetadataKeys } from "./MetadataKeys";
+import { Methods } from "./Methods";
 
-export function get(path: string) {
-  return function (target: any, key: string, desc: PropertyDescriptor) {
-    Reflect.defineMetadata("path", path, target, key);
+function routeBinder(method: string) {
+  return function (path: string) {
+    /**
+     * target: { getLogin: [Function (anonymous)] }
+     * key: 'getLogin', ...
+     */
+    return function (target: any, key: string, desc: PropertyDescriptor) {
+      Reflect.defineMetadata(MetadataKeys.path, path, target, key);
+      Reflect.defineMetadata(MetadataKeys.method, method, target, key);
+    };
   };
 }
+
+export const get = routeBinder(Methods.get);
+export const put = routeBinder(Methods.put);
+export const post = routeBinder(Methods.post);
+export const del = routeBinder(Methods.del);
+export const patch = routeBinder(Methods.patch);
