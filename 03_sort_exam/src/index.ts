f60cc6b9c9fe3dbde054ae46f181551d70c38a82
orchsik <orchsik@naver.com>
2022-02-15 20:09:05 +0900

    Add: 정렬 팩토리 함수

--- 03_sort_exam/src/index.ts
+++ 03_sort_exam/src/index.ts
@@ -8,3 +8,20 @@ console.log(charactersCollection.collection);
 const numbersCollection = new NumbersCollection([3, 0, 100, -2]);
 numbersCollection.sort();
 console.log(numbersCollection.collection);
+
+/**
+ * 정렬 팩토리 함수
+ */
+const sort = (collection: any) => {
+  switch (typeof collection[0]) {
+    case "number":
+      new NumbersCollection(collection).sort();
+      break;
+    case "string":
+      new CharactersCollection(collection).sort();
+      break;
+    default:
+      throw new Error("정렬을 지원하지 않는 타입 입니다.");
+  }
+};
+sort([3, 0, 100, -2]);
