7add3199374db112697b6ed289032520590358a9
orchsik <orchsik@naver.com>
2022-02-15 21:42:44 +0900

    Update: routes

--- 06_server/src/controllers/LoginController.ts
+++ 06_server/src/controllers/LoginController.ts
@@ -38,4 +38,10 @@ export class LoginController {
       res.send("Invalid email or password");
     }
   }
+
+  @get("/logout")
+  getLogout(req: Request, res: Response) {
+    req.session = undefined;
+    res.redirect("/");
+  }
 }
--- 06_server/src/index.ts
+++ 06_server/src/index.ts
@@ -2,15 +2,14 @@ import express from "express";
 import bodyParser from "body-parser";
 import cookieSession from "cookie-session";
 
-import { router } from "./routes/loginRoutes";
 import "./controllers/LoginController";
+import "./controllers/RootController";
 import { AppRouter } from "./AppRouter";
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({ keys: ["aaa"] }));
-app.use(router);
 app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
