02db534befd6c7068e6552da524840220b591307
orchsik <orchsik@naver.com>
2022-02-15 21:04:40 +0900

    Feat: Add decorator "use"

--- 06_server/src/controllers/LoginController.ts
+++ 06_server/src/controllers/LoginController.ts
@@ -1,9 +1,15 @@
-import { Request, Response } from "express";
-import { controller, get } from "./decorators";
+import { NextFunction, Request, Response } from "express";
+import { controller, get, use } from "./decorators";
+
+function logger(req: Request, res: Response, next: NextFunction) {
+  console.log("Request was made!!!");
+  next();
+}
 
 @controller("/auth")
 export class LoginController {
   @get("/login")
+  @use(logger)
   getLogin(req: Request, res: Response): void {
     res.send(`
         <form method="post">
